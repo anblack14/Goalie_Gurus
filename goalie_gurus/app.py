9440faed5c82f7e4149b4a1de11fde7aa2ca1147
Alex Black <anblack14@att.net>
2020-03-13 19:43:58 -0400

    Heroku

--- goalie_gurus/app.py
+++ goalie_gurus/app.py
@@ -36,72 +36,72 @@ conn = engine.connect()
 app = Flask(__name__)
 
 
-# # Set up your default route
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
+# Set up your default route
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
-# @app.route('/api/data/current_scorers_data')
-# def getcurrent_scorers_data():
-#     # Establish DB connection
-#     conn = engine.connect()
-#     try:
-#         data = pd.read_sql("SELECT * FROM top56_scorers_GL_data ", conn)
-#         print("Connected")
-#         return data.to_json(orient='records')
-#     except Exception as e:
-#         print(e)
-#         return render_template('error.html', error=True)
+@app.route('/api/data/current_scorers_data')
+def getcurrent_scorers_data():
+    # Establish DB connection
+    conn = engine.connect()
+    try:
+        data = pd.read_sql("SELECT * FROM top56_scorers_GL_data ", conn)
+        print("Connected")
+        return data.to_json(orient='records')
+    except Exception as e:
+        print(e)
+        return render_template('error.html', error=True)
 
 
-# @app.route('/api/data/historic_data')
-# def gethistoric_data():
-#     # Establish DB connection
-#     conn = engine.connect()
-#     try:
-#         data = pd.read_sql("SELECT * FROM hr_data ", conn)
-#         return data.to_json(orient='records')
-#     except Exception as e:
-#         print(e)
-#         return render_template('error.html', error=True)
+@app.route('/api/data/historic_data')
+def gethistoric_data():
+    # Establish DB connection
+    conn = engine.connect()
+    try:
+        data = pd.read_sql("SELECT * FROM hr_data ", conn)
+        return data.to_json(orient='records')
+    except Exception as e:
+        print(e)
+        return render_template('error.html', error=True)
 
 
-# @app.route('/api/data/player_data')
-# def getplayer_data():
-#     # Establish DB connection
-#     conn = engine.connect()
-#     try:
-#         data = pd.read_sql("SELECT * FROM player_data ", conn)
-#         return data.to_json()
-#     except Exception as e:
-#         print(e)
-#         return render_template('error.html', error=True)
+@app.route('/api/data/player_data')
+def getplayer_data():
+    # Establish DB connection
+    conn = engine.connect()
+    try:
+        data = pd.read_sql("SELECT * FROM player_data ", conn)
+        return data.to_json()
+    except Exception as e:
+        print(e)
+        return render_template('error.html', error=True)
 
 
-# @app.route('/api/data/scorers_data')
-# def getscorers_data():
-#     # Establish DB connection
-#     conn = engine.connect()
-#     try:
-#         data = pd.read_sql("SELECT * FROM scorers_data ", conn)
-#         return data.to_json()
-#     except Exception as e:
-#         print(e)
-#         return render_template('error.html', error=True)
+@app.route('/api/data/scorers_data')
+def getscorers_data():
+    # Establish DB connection
+    conn = engine.connect()
+    try:
+        data = pd.read_sql("SELECT * FROM scorers_data ", conn)
+        return data.to_json()
+    except Exception as e:
+        print(e)
+        return render_template('error.html', error=True)
 
 
-# @app.route('/api/data/stats_data')
-# def getstats_data():
-#     # Establish DB connection
-#     conn = engine.connect()
-#     try:
-#         data = pd.read_sql("SELECT * FROM stats_data ", conn)
-#         return data.to_json()
-#     except Exception as e:
-#         print(e)
-#         return render_template('error.html', error=True)
+@app.route('/api/data/stats_data')
+def getstats_data():
+    # Establish DB connection
+    conn = engine.connect()
+    try:
+        data = pd.read_sql("SELECT * FROM stats_data ", conn)
+        return data.to_json()
+    except Exception as e:
+        print(e)
+        return render_template('error.html', error=True)
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
